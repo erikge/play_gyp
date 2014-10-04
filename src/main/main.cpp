47e7b65178d315a33cf53fe6cdef27343bb5f191
erikge <erikge@qq.com>
2014-10-04 15:08:05 +0800

    添加循环打印，验证Xcode的 "Debug->Attach to Process"可以工作

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -1,7 +1,15 @@
 #include <stdio.h>
+#include <unistd.h>
 
 
 int main(int argc, const char* argv[]) {
     printf("Hello GYP\n");
+    
+    int i = 0;
+    while (true) {
+        printf("Hello GYP: %d \n", i++);
+        sleep(1);
+    }
+
     
 }
